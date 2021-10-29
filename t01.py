3ddba098e35b4580a0588355c42e7e96a1dd994f
Zobeir Raisi <zobeir.raisi@gmail.com>
2021-10-29 18:52:28 -0400

    Update and rename list_linked.py to t01.py

---
+++ t01.py
@@ -0,0 +1,22 @@
+from List_linked import List
+l = List()
+l.append(44)
+
+t1,t2 = l.split()
+
+
+
+#l.clean()
+for i in t1:
+    print(i)
+print()
+for j in t2:
+    print(j)
+'''
+val = l.remove_front()
+for i in l:
+    print(i)
+    
+print()
+print(val)
+'''
