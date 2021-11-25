3d7f26615752bcd8c030b85386d344753a2e48ef
Zobeir Raisi <zobeir.raisi@gmail.com>
2021-11-24 19:35:06 -0500

    Update and rename morse.py to Hash_Set_BST.py

---
+++ Hash_Set_BST.py
@@ -0,0 +1,230 @@
+"""
+# Imports
+# Use any appropriate data structure here.
+from BST_linked import BST
+# Define the new_slot slot creation function.
+new_slot = BST
+
+# Constants
+SEP = '-' * 40
+
+
+class Hash_Set:
+    """
+    -------------------------------------------------------
+    Constants.
+    -------------------------------------------------------
+    """
+    _LOAD_FACTOR = 20
+
+    def __init__(self, slots):
+        """
+        -------------------------------------------------------
+        Initializes an empty Hash_Set of size slots.
+        Use: hs = Hash_Set(slots)
+        -------------------------------------------------------
+        Parameter:
+            slots - number of initial slots in Hash Set (int > 0)
+        Returns:
+            A new Hash_Set object (Hash_Set)
+        -------------------------------------------------------
+        """
+        self._slots = slots
+        self._table = []
+        self._count = 0
+
+        # Define the empty slots.
+        for _ in range(self._slots):
+            self._table.append(new_slot())
+
+    def __len__(self):
+        """
+        -------------------------------------------------------
+        Returns the number of values in the Hash Set.
+        Use: n = len(hs)
+        -------------------------------------------------------
+        Returns:
+            the number of values in the Hash Set.
+        -------------------------------------------------------
+        """
+        return self._count
+
+    def is_empty(self):
+        """
+        -------------------------------------------------------
+        Determines if the Hash Set is empty.
+        Use: b = hs.is_empty()
+        -------------------------------------------------------
+        Returns:
+            True if the Hash Set is empty, False otherwise.
+        -------------------------------------------------------
+        """
+        return self._count == 0
+
+    def _find_slot(self, key):
+        """
+        -------------------------------------------------------
+        Returns the slot for a key value.
+        Use: list = hs._find_slot(key)
+        -------------------------------------------------------
+        Returns:
+            slot - list at the position of hash key in self._table
+        -------------------------------------------------------
+        """
+        return self._table[hash(key) % self._slots]
+
+
+    def __contains__(self, key):
+        """
+        ---------------------------------------------------------
+        Determines if the Hash Set contains key.
+        Use: b = key in hs
+        -------------------------------------------------------
+        Parameters:
+            key - a comparable data element (?)
+        Returns:
+            True if the Hash Set contains key, False otherwise.
+        -------------------------------------------------------
+        """
+
+        # your code here
+
+
+    def insert(self, value):
+        """
+        ---------------------------------------------------------
+        Inserts value into the Hash Set, allows only one copy of value.
+        Calls _rehash if the Hash Set _LOAD_FACTOR is exceeded.
+        Use: inserted = hs.insert(value)
+        -------------------------------------------------------
+        Parameters:
+            value - a comparable data element (?)
+        Returns:
+            inserted - True if value is inserted, False otherwise.
+        -------------------------------------------------------
+        """
+        slot = self._find_slot(value)
+        if value in slot:
+            insert = False
+        else:
+            if self._count >= self._slots * Hash_Set._LOAD_FACTOR:
+                self._rehash()
+            slot.insert(value)
+            insert = True
+            self._count += 1
+        return insert
+
+    def find(self, key):
+        """
+        ---------------------------------------------------------
+        Returns the value identified by key.
+        Use: value = hs.find(key)
+        -------------------------------------------------------
+        Parameters:
+            key - a comparable data element (?)
+        Returns:
+            value - if it exists in the Hash Set, None otherwise.
+        -------------------------------------------------------
+        """
+
+        # your code here
+
+    def remove(self, key):
+        """
+        ---------------------------------------------------------
+        Removes the value matching key from the Hash Set, if it exists.
+        Use: value = hs.remove(key)
+        -------------------------------------------------------
+        Parameters:
+            key - a comparable data element (?)
+        Returns:
+            value - if it exists in the Hash Set, None otherwise.
+        -------------------------------------------------------
+        """
+        slot = self._find_slot(key)
+        if key not in slot:
+            value = None
+        i = 0
+        while i < len(slot):
+            if slot[i] == key:
+                value = slot.pop(i)
+            i += 1
+        return value
+
+    def _rehash(self):
+        """
+        ---------------------------------------------------------
+        Increases the number of slots in the Hash Set and reallocates the
+        existing data within the Hash Set to the new table.
+        Use: hs._rehash()
+        -------------------------------------------------------
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        self._slots = 2 * self._slots + 1
+        temp = []
+        for _ in range(self._slots):
+            temp.append(new_slot())
+        for slot in self._table:
+            for value in slot:
+                slot_num = hash(value) % self._slots
+                temp[slot_num].insert(value)
+        self._table = temp
+        return
+
+    def is_identical(self, target):
+        """
+        ---------------------------------------------------------
+        Determines whether two hash sets are identical.
+        Use: b = source.is_identical(target)
+        -------------------------------------------------------
+        Parameters:
+             target - another hash set (Hash_Set)
+        Returns:
+            identical - True if this hash set contains the same values 
+                as other in the same order, otherwise returns False.
+        -------------------------------------------------------
+        """
+
+        # your code here
+
+
+    def debug(self):
+        """
+        USE FOR TESTING ONLY
+        ---------------------------------------------------------
+        Prints the contents of the Hash Set starting at slot 0,
+        showing the slot currently being printed. Used for
+        debugging purposes.
+        Use: hs.debug()
+        -------------------------------------------------------
+        Returns:
+            None
+        -------------------------------------------------------
+        """
+        print("{} slots".format(self._slots))
+        print()
+        for slot_num in range(self._slots):
+            print('========================================')
+            print('Slot {}'.format(slot_num))
+            for value in self._table[slot_num]:
+                print(value)
+        return
+
+    def __iter__(self):
+        """
+        USE FOR TESTING ONLY
+        -------------------------------------------------------
+        Generates a Python iterator. Iterates through the hash set
+        from first to last slots. Assumes slot has own iterator.
+        Use: for v in q:
+        -------------------------------------------------------
+        Returns:
+            yields
+            value - the next value in the list (?)
+        -------------------------------------------------------
+        """
+        for slot in self._table:
+            for item in slot:
+                yield item
